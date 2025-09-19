dd7fd73f619aeb99afe15579deb96a97a3132dd7
turtle-nest <n78693107@gmail.com>
2025-09-19 11:43:31 +0200

    chore(frontend): install and config Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      container: { center: true, padding: "1rem" }
+    },
+  },
+  plugins: [],
+};
